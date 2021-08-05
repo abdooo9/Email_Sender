65553af7253208bdd17b5692efcaab98ff6f43bf
abdooo9 <76847516+abdooo9@users.noreply.github.com>
2021-08-05 14:30:31 +0300

    First Upload

---
+++ index.js
@@ -0,0 +1,19 @@
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  auth: {
+    user: "",
+    pass: "",
+  },
+});
+
+transporter.sendMail({
+  from: "", // sender address
+  to: "", // list of receivers
+  subject: "", // Subject line
+  text: "", // plain text body
+  // html: , // html body
+}).then(info => {
+  console.log({ info });
+}).catch(console.error);
